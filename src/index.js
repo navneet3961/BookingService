5bbeef98f465016bf92d8b2edeb3d07b1b8d271f
Navneet Varshney <navarshney99@gmail.com>
2023-03-23 23:39:23 +0530

    Added error management files to the initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serviceConfig");
+const apiRoutes = require("./routes/index");
 const db = require('./models/index');
 
 const setupAndStartServer = async () => {
     const app = new express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use("/api", apiRoutes);
 
     app.listen(PORT, async () => {
         console.log(`Server has started at ${PORT}`);
